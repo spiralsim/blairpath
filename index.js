e62b36a0229b5b2e2cf7d1e6fb10d4886a9855aa
csytrn <jeffrey.w.tong@gmail.com>
2021-04-17 00:03:34 -0400

    Move Blairpath to its own domain, make old Replit site obsolete, and rewrite Home and About pages

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const express = require("express"), fs = require("fs");
+const express = require("express"), http = require("http"), fs = require("fs");
 
+// Setup
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 app
-	//.use("/favicon.ico", express.static(`${__dirname}/assets/favicon.ico`))
 	.use(express.static(`${__dirname}/assets`))
 	.set("views", `${__dirname}/views`)
 	.set("view engine", "ejs")
 	.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-app.get(/.*/, (req, res, next) => {
+// Handle all asset and page requests
+app.get(/.*/, (req, res) => {
 	const path = req.path;
 	if (path == '/') res.redirect("/home");
 	else if (fs.existsSync(`${__dirname}/views/pages/${path}.ejs`)) res.render(`pages/${path}`);
 	else res.render("pages/404");
-	next();
 });
-app.use((req, res) => console.log(`${req.method} ${req.path}`));
+
+// Ping every 15 minutes to keep the dyno running
+setInterval(() => http.get("http://blairpath.org"), 60 * 1000);
